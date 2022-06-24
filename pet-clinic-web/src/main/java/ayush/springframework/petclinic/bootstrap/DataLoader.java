d25efced0019effa47e7e9b4aca11db051ab2dd8
Ayush <lionelayush446@gmail.com>
2022-06-24 11:02:15 +0530

    Added Vets list and updated controller(Adding model). Closes #14.

--- pet-clinic-web/src/main/java/ayush/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ayush/springframework/petclinic/bootstrap/DataLoader.java
@@ -43,9 +43,9 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet1.setId(2L);
-        vet1.setFirstName("Jessie");
-        vet1.setLastName("Porter");
+        vet2.setId(2L);
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
         vetService.save(vet2);
 
         System.out.println("Loaded vets");
--- pet-clinic-web/src/main/java/ayush/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ayush/springframework/petclinic/controllers/VetController.java
@@ -1,12 +1,21 @@
 package ayush.springframework.petclinic.controllers;
 
+import ayush.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets" , vetService.findAll());
         return "vets/index";
     }
 }
