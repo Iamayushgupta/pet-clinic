34a4d57abdf717d81ed98fe634e4374f420694c5
Ayush <lionelayush446@gmail.com>
2022-07-13 12:31:00 +0530

    Managing setting of ID property

--- pet-clinic-data/src/main/java/ayush/springframework/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/ayush/springframework/petclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package ayush.springframework.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import ayush.springframework.petclinic.model.BaseEntity;
 
-public class AbstractMapService <T , ID>{
-    protected Map<ID,T> map = new HashMap<>();
+import java.util.*;
+
+public class AbstractMapService <T extends BaseEntity, ID extends Long>{
+    protected Map<Long,T> map = new HashMap<>();
     Set<T> findAll(){
         return new HashSet<>(map.values());
     }
@@ -14,10 +13,20 @@ public class AbstractMapService <T , ID>{
         return map.get(id);
     }
 
-    T save(ID id,T object){
-        map.put(id , object);
+    T save(T object){
+        if (object!=null){
+            if(object.getId()==null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }
+        else{
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
+
+
 
     void deleteById(ID id){
         map.remove(id);
@@ -25,6 +34,17 @@ public class AbstractMapService <T , ID>{
 
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId=null;
+        try{
+            nextId=Collections.max(map.keySet())+1;
+        }
+        catch(NoSuchElementException e){
+            nextId=1L;
+        }
+        return nextId;
     }
 
 }
