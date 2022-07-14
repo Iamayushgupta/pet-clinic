428bf29cb19a64e105329d1d6d05481517b38d92
Ayush <lionelayush446@gmail.com>
2022-07-14 12:25:10 +0530

    Added Speciality. Closes #24.

--- pet-clinic-data/src/main/java/ayush/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ayush/springframework/petclinic/model/Vet.java
@@ -1,11 +1,15 @@
 package ayush.springframework.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
-    public Vet(String firstName, String lastName) {
-        super(firstName, lastName);
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
     }
 
-    public Vet() {
-        super();
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
     }
 }
